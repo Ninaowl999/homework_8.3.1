feee25a9a93b6cb670569f1b5f6e066141a5f35e
Ninaowl999 <128140719+Ninaowl999@users.noreply.github.com>
2023-12-20 23:14:46 +0400

    Add commit Main.java, ChinaBattery.java, Flashlight.java.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Flashlight flashlight = new Flashlight();
+
+        System.out.println("Flashlight is initially off: " + !flashlight.isLight());
+
+        flashlight.on();
+        System.out.println("Flashlight is on: " + flashlight.isLight());
+
+        flashlight.off();
+        System.out.println("Flashlight is off: " + !flashlight.isLight());
+
+        flashlight.on();
+        flashlight.on();
+        flashlight.on();
+        flashlight.on();
+        flashlight.on();
+        System.out.println("Flashlight is on: " + flashlight.isLight());
     }
 }
